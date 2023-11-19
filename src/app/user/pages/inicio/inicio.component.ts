c199b899085f1e85c9f91420ca170342759e31d0
ElNao <luis28jair5@gmail.com>
2023-11-19 11:53:59 -0600

    nuevos

--- src/app/user/pages/inicio/inicio.component.ts
+++ src/app/user/pages/inicio/inicio.component.ts
@@ -11,4 +11,31 @@ export class InicioComponent {
   'https://www.tooltyp.com/wp-content/uploads/2014/10/1900x920-8-beneficios-de-usar-imagenes-en-nuestros-sitios-web.jpg',
   'https://png.pngtree.com/thumb_back/fh260/background/20230611/pngtree-wolf-animals-images-wallpaper-for-pc-384x480-image_2916211.jpg',
 ];
+
+public card = [{
+  titulo:'Pantalon para caballero',
+  precio:'$500.00',
+  img:'https://st.depositphotos.com/1016440/2534/i/450/depositphotos_25344733-stock-photo-sunrise-at-the-beach.jpg',
+  descripccion:'Pantalon para caballero',
+},
+{
+  titulo:'Pantalon para caballero',
+  precio:'$500.00',
+  img:'https://st.depositphotos.com/1016440/2534/i/450/depositphotos_25344733-stock-photo-sunrise-at-the-beach.jpg',
+  descripccion:'Pantalon para caballero',
+},
+{
+  titulo:'Pantalon para caballero',
+  precio:'$500.00',
+  img:'https://st.depositphotos.com/1016440/2534/i/450/depositphotos_25344733-stock-photo-sunrise-at-the-beach.jpg',
+  descripccion:'Pantalon para caballero',
+},
+{
+  titulo:'Pantalon para caballero',
+  precio:'$500.00',
+  img:'https://st.depositphotos.com/1016440/2534/i/450/depositphotos_25344733-stock-photo-sunrise-at-the-beach.jpg',
+  descripccion:'Pantalon para caballero',
+},
+]
+
 }
